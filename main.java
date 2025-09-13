6cfe1e7ccf31f23d6781416734a9cad1d2785834
William Yuan <86529144+Fire-Surveillance@users.noreply.github.com>
2025-09-13 14:39:56 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,15 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println(x:"Hello World");
+public class main{
+    public static void main(String[] args){
+
+        int n = 365;
+        int sum = 0;
+        while (n>0){
+            int digit = n%10;
+            sum = sum+digit;
+            n=n/10;
+        }
+
+        System.out.println(sum);
     }
 }
+
